e1e44006c60808a8de65bc26ee479084655f1115
gaalszaby <gaalszaby@outlook.com>
2022-10-07 14:25:09 +0200

    First commit of the app

---
+++ src/components/TotalBudgetCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { useBudgets } from "../contexts/BudgetsContext";
+import BudgetCard from "./BudgetCard";
+
+const TotalBudgetCard = (props) => {
+  const { expenses, budgets } = useBudgets();
+
+  const amount = expenses.reduce((total, expense) => total + expense.amount, 0);
+  const max = budgets.reduce((total, budget) => total + budget.max, 0);
+
+  if (max === 0) {
+    return null;
+  }
+
+  return <BudgetCard gray name="Total" amount={amount} max={max} hideButtons />;
+};
+
+export default TotalBudgetCard;
---
+++ src/utils.js
@@ -0,0 +1,5 @@
+export const currencyFormatter = new Intl.NumberFormat(undefined, {
+  currency: "HUF",
+  style: "currency",
+  minimumFractionDigits: 0,
+});
